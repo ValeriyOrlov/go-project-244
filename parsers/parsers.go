6b294bb4abdcc876a75076a70446af957ab795bd
Valerrriy Orlov <wrestlbird@gmail.com>
2025-12-23 23:19:03 +0300

    fix: adjusted the output of parsed data

--- parsers/parsers.go
+++ parsers/parsers.go
@@ -16,22 +16,26 @@ func fileReader(path string) ([]byte, error) {
 	return data, nil
 }
 
-func Parser(fp string) {
+func Parser(fp string) (map[string]any, error) {
 	f, err := fileReader(fp)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if strings.HasSuffix(fp, "json") {
-		jsonParser(f)
+		res, err := jsonParser(f)
+		if err != nil {
+			return nil, fmt.Errorf("cannot read file: %w", err)
+		}
+		return res, nil
 	}
+	return nil, nil
 }
 
-func jsonParser(data []byte) error {
-	var result any
+func jsonParser(data []byte) (map[string]any, error) {
+	var result map[string]any
 	err := json.Unmarshal(data, &result)
 	if err != nil {
-		return fmt.Errorf("cannot parse file: %w", err)
+		return nil, fmt.Errorf("cannot parse file: %w", err)
 	}
-	fmt.Printf("%+v\n", result)
-	return nil
+	return result, nil
 }

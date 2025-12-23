944201154b4047515902d052bf805cf237d066ba
Valerrriy Orlov <wrestlbird@gmail.com>
2025-12-23 23:30:04 +0300

    feat: the gendiff function is implemented to search and aggregate the differences between two flat (key-value pairs only) json files.added console output of the gendiff function result

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"code/formatters"
+	"code/parsers"
 	"context"
 	"fmt"
 	"log"
+	"maps"
 	"os"
+	"slices"
 
 	"github.com/urfave/cli/v3" // imports as package "cli"
 )
+
+func gendiff(data1, data2 map[string]any) map[string]string {
+	const (
+		equal      = " "
+		rowOfData1 = "-"
+		rowOfData2 = "+"
+	)
+
+	diff := make(map[string]string)
+	data1Keys := slices.Sorted(maps.Keys(data1))
+	data2Keys := slices.Sorted(maps.Keys(data2))
+	for key, value := range data1 {
+		if slices.Contains(data2Keys, key) && data2[key] == value {
+			row := fmt.Sprintf("%s: %v", key, value)
+			diff[row] = equal
+		} else if slices.Contains(data2Keys, key) && data2[key] != value {
+			row1 := fmt.Sprintf("%s: %v", key, value)
+			row2 := fmt.Sprintf("%s: %v", key, data2[key])
+			diff[row1] = rowOfData1
+			diff[row2] = rowOfData2
+		} else {
+			row := fmt.Sprintf("%s: %v", key, value)
+			diff[row] = rowOfData1
+		}
+	}
+	for key := range data2 {
+		if !slices.Contains(data1Keys, key) {
+			row := fmt.Sprintf("%s: %v", key, data2[key])
+			diff[row] = rowOfData2
+		}
+	}
+	return diff
+}
 
 func main() {
 	cmd := &cli.Command{
@@ -24,7 +61,19 @@ func main() {
 			if cmd.Bool("format") {
 				fmt.Println("test format")
 			}
-			fmt.Println("test start")
+
+			if cmd.Args().Len() < 2 {
+				fmt.Println("file paths are not specified (use the -h flag for reference)")
+				return nil
+			}
+			path1 := cmd.Args().Get(0)
+			path2 := cmd.Args().Get(1)
+
+			data1, _ := parsers.Parser(path1)
+			data2, _ := parsers.Parser(path2)
+			diff := gendiff(data1, data2)
+			result := formatters.Stylish(diff)
+			fmt.Println(result)
 			return nil
 		},
 	}

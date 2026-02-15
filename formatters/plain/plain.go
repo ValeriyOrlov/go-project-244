1ea86da31e6fd02fca379b7037f5a7fe0071a7cb
Valerrriy Orlov <wrestlbird@gmail.com>
2026-02-15 12:22:50 +0300

    add/ref: added tests. the formatter has been rewritten to fit the updated slice structure

--- formatters/plain/plain.go
+++ formatters/plain/plain.go
@@ -3,67 +3,61 @@ package formatters
 import (
 	"code/cmd/gendiff"
 	"fmt"
-	"maps"
-	"reflect"
-	"slices"
 	"strings"
 )
 
-func IsBool(v interface{}) bool {
+func normalizeValue(v any) string {
 	if v == nil {
-		return false
+		return "null"
 	}
-	return reflect.TypeOf(v).Kind() == reflect.Bool
+	switch val := v.(type) {
+	case bool:
+		return fmt.Sprintf("%v", val)
+	case string:
+		return fmt.Sprintf("'%s'", val)
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64:
+		return fmt.Sprintf("%v", val)
+	default:
+		// Для составных типов (map, slice, struct) возвращаем [complex value]
+		return "[complex value]"
+	}
 }
 
-func Plain(data map[string]gendiff.KeyCharacteristics, path []string) string {
+func Plain(data []gendiff.KeyCharacteristics, path []string) string {
 	result := []string{}
-	keys := slices.Sorted(maps.Keys(data))
-	keyStatuses := map[string]string{"added": " was added with value: ", "deleted": " was removed", "changed": " was updated. "}
-	var removedVal interface{}
-	var addedVal interface{}
-	complexVal := "[complex value]"
+	keyStatuses := map[string]string{
+		"added":   " was added with value: ",
+		"deleted": " was removed",
+		"changed": " was updated. ",
+	}
 	propertyStr := "Property "
-	for _, key := range keys {
-		if data[key].HasDiff {
-			path = append(path, data[key].Key, ".")
-			result = append(result, Plain(data[key].Value.(map[string]gendiff.KeyCharacteristics), path))
-			if len(path) != 0 {
-				path = path[0 : len(path)-2]
-			} else {
-				path = []string{}
-			}
-		} else {
-			switch data[key].Status {
-			case "added":
-				if gendiff.IsMap(data[key].Value) {
-					addedVal = complexVal
-				} else if IsBool(data[key].Value) || data[key].Value == nil {
-					addedVal = fmt.Sprintf(`%v`, data[key].Value)
-				} else {
-					addedVal = fmt.Sprintf(`'%v'`, data[key].Value)
-				}
-				result = append(result, propertyStr, fmt.Sprintf(`'%s%s'`, strings.Join(path, ""), data[key].Key), keyStatuses[data[key].Status], fmt.Sprint(addedVal), "\n")
-			case "deleted":
-				result = append(result, propertyStr, fmt.Sprintf(`'%s%s'`, strings.Join(path, ""), data[key].Key), keyStatuses[data[key].Status], "\n")
-			case "changed":
-				if gendiff.IsMap(data[key].Value.([]gendiff.KeyCharacteristics)[0].Value) {
-					removedVal = complexVal
-				} else if IsBool(data[key].Value.([]gendiff.KeyCharacteristics)[0].Value) || data[key].Value.([]gendiff.KeyCharacteristics)[0].Value == nil {
-					removedVal = fmt.Sprintf(`%v`, data[key].Value.([]gendiff.KeyCharacteristics)[0].Value)
-				} else {
-					removedVal = fmt.Sprintf(`'%v'`, data[key].Value.([]gendiff.KeyCharacteristics)[0].Value)
-				}
-				if gendiff.IsMap(data[key].Value.([]gendiff.KeyCharacteristics)[1].Value) {
-					addedVal = complexVal
-				} else if IsBool(data[key].Value.([]gendiff.KeyCharacteristics)[1].Value) || data[key].Value.([]gendiff.KeyCharacteristics)[1].Value == nil {
-					addedVal = fmt.Sprintf(`%v`, data[key].Value.([]gendiff.KeyCharacteristics)[1].Value)
-				} else {
-					addedVal = fmt.Sprintf(`'%v'`, data[key].Value.([]gendiff.KeyCharacteristics)[1].Value)
-				}
-				changedValue := fmt.Sprintf("From %v to %v.", removedVal, addedVal)
-				result = append(result, propertyStr, fmt.Sprintf(`'%s%s'`, strings.Join(path, ""), data[key].Key), keyStatuses[data[key].Status], changedValue, "\n")
-			}
+
+	for i, key := range data {
+		var addedValue, removedValue any
+		var nextKey, prevKey string
+		if i > 0 {
+			prevKey = data[i-1].Name
+		}
+		if i < len(data)-1 {
+			nextKey = data[i+1].Name
+		}
+
+		switch {
+		case key.Status == "changed":
+			newPath := append(append([]string{}, path...), key.Name, ".")
+			result = append(result, Plain(key.Value.([]gendiff.KeyCharacteristics), newPath))
+		case key.Status == "deleted" && key.Name != nextKey:
+			result = append(result, propertyStr, fmt.Sprintf(`'%s%s'`, strings.Join(path, ""), key.Name), keyStatuses[key.Status], "\n")
+
+		case key.Status == "added" && key.Name != prevKey:
+			addedValue = normalizeValue(key.Value)
+			result = append(result, propertyStr, fmt.Sprintf(`'%s%s'`, strings.Join(path, ""), key.Name), keyStatuses[key.Status], addedValue.(string), "\n")
+
+		case key.Status == "added" && key.Name == prevKey:
+			removedValue = normalizeValue(data[i-1].Value)
+			addedValue = normalizeValue(key.Value)
+			changedValues := fmt.Sprintf("From %s to %s", removedValue, addedValue)
+			result = append(result, propertyStr, fmt.Sprintf(`'%s%s'`, strings.Join(path, ""), key.Name), keyStatuses["changed"], changedValues, "\n")
 		}
 	}
 	return strings.Join(result, "")
---
+++ formatters/plain/plain_test.go
@@ -0,0 +1,120 @@
+package formatters
+
+import (
+	"code/cmd/gendiff"
+	"testing"
+)
+
+func TestPlain(t *testing.T) {
+	tests := []struct {
+		name     string
+		data     []gendiff.KeyCharacteristics
+		expected string
+	}{
+		{
+			name: "added simple value",
+			data: []gendiff.KeyCharacteristics{
+				{Name: "key1", Status: "added", Value: "value1"},
+			},
+			expected: "Property 'key1' was added with value: 'value1'\n",
+		},
+		{
+			name: "deleted simple value",
+			data: []gendiff.KeyCharacteristics{
+				{Name: "key1", Status: "deleted", Value: "value1"},
+			},
+			expected: "Property 'key1' was removed\n",
+		},
+		{
+			name: "updated simple value (deleted+added pair)",
+			data: []gendiff.KeyCharacteristics{
+				{Name: "key1", Status: "deleted", Value: "old"},
+				{Name: "key1", Status: "added", Value: "new"},
+			},
+			expected: "Property 'key1' was updated. From 'old' to 'new'\n",
+		},
+		{
+			name: "updated with boolean and null",
+			data: []gendiff.KeyCharacteristics{
+				{Name: "flag", Status: "deleted", Value: true},
+				{Name: "flag", Status: "added", Value: nil},
+			},
+			expected: "Property 'flag' was updated. From true to null\n",
+		},
+		{
+			name: "updated with complex value",
+			data: []gendiff.KeyCharacteristics{
+				{Name: "obj", Status: "deleted", Value: map[string]any{"a": 1}},
+				{Name: "obj", Status: "added", Value: "simple"},
+			},
+			expected: "Property 'obj' was updated. From [complex value] to 'simple'\n",
+		},
+		{
+			name: "nested object with added property",
+			data: []gendiff.KeyCharacteristics{
+				{
+					Name:   "common",
+					Status: "changed",
+					Value: []gendiff.KeyCharacteristics{
+						{Name: "setting1", Status: "added", Value: "value1"},
+					},
+				},
+			},
+			expected: "Property 'common.setting1' was added with value: 'value1'\n",
+		},
+		{
+			name: "nested object with updated property",
+			data: []gendiff.KeyCharacteristics{
+				{
+					Name:   "common",
+					Status: "changed",
+					Value: []gendiff.KeyCharacteristics{
+						{Name: "setting2", Status: "deleted", Value: "old"},
+						{Name: "setting2", Status: "added", Value: "new"},
+					},
+				},
+			},
+			expected: "Property 'common.setting2' was updated. From 'old' to 'new'\n",
+		},
+		{
+			name: "deep nested path",
+			data: []gendiff.KeyCharacteristics{
+				{
+					Name:   "a",
+					Status: "changed",
+					Value: []gendiff.KeyCharacteristics{
+						{
+							Name:   "b",
+							Status: "changed",
+							Value: []gendiff.KeyCharacteristics{
+								{Name: "c", Status: "added", Value: 42},
+							},
+						},
+					},
+				},
+			},
+			expected: "Property 'a.b.c' was added with value: 42\n",
+		},
+		{
+			name: "multiple changes at root",
+			data: []gendiff.KeyCharacteristics{
+				{Name: "key1", Status: "deleted", Value: 1},
+				{Name: "key1", Status: "added", Value: 2},
+				{Name: "key2", Status: "added", Value: "new"},
+				{Name: "key3", Status: "deleted", Value: "old"},
+			},
+			expected: "Property 'key1' was updated. From 1 to 2\n" +
+				"Property 'key2' was added with value: 'new'\n" +
+				"Property 'key3' was removed\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := Plain(tt.data, []string{})
+			if result != tt.expected {
+				t.Errorf("expected:\n%q\ngot:\n%q", tt.expected, result)
+			}
+		})
+	}
+}

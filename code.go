81be5ab8d1dca7310326c6425a8fbc5c86e3ed3e
Valerrriy Orlov <wrestlbird@gmail.com>
2026-02-15 15:44:39 +0300

    add GenDiff wrapper function

--- code.go
+++ code.go
@@ -1,1 +1,8 @@
 package code
+
+import "code/formatters"
+
+// GenDiff сравнивает два файла конфигурации и возвращает разницу в заданном формате.
+func GenDiff(filepath1, filepath2, format string) (string, error) {
+	return formatters.Format(filepath1, filepath2, format)
+}
